bb1688b93a43584509a5d2753e688b6ff5b59343
rosy871 <yalameju@gmail.com>
2021-06-15 16:28:10 +0200

    Add project files.

---
+++ Register/PersonalRegister.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Register
+{
+    class PersonalRegister
+    {
+        
+        string namn;
+        int lön;
+
+        public void setNamn(string name)
+        {
+            namn = name;
+        }
+
+        public string getNamn()
+        {
+            return namn;
+        }
+
+        public void setLon(int salary)
+        {
+            lön = salary;
+        }
+
+        public int getLon()
+        {
+            return lön;
+        }
+
+
+
+    }
+}
---
+++ Register/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Register
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<PersonalRegister> regs = new List<PersonalRegister>();
+            
+            
+            int ch=1;
+            do {
+
+                if (ch == 1)
+                {
+               
+                    Console.WriteLine("Enter name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("\nEnter salary: ");
+                    int salary = Convert.ToInt32(Console.ReadLine());
+                    PersonalRegister reg = new PersonalRegister();
+
+                    reg.setNamn(name);
+                    reg.setLon(salary);
+
+
+
+                    regs.Add(reg);
+
+                }
+                else if(ch == 2) {
+
+                 
+                    foreach (var person in regs)
+                        Console.WriteLine(person.getNamn() + "\t " + person.getLon());
+
+
+                }
+                              
+                Console.WriteLine("\nPress 1 to Enter Personal info:");
+                Console.WriteLine("Press 2 to list Personal info:");
+                Console.WriteLine("Press 3 to Exit Personal info:");
+                ch = Convert.ToInt32(Console.ReadLine());
+            } while (ch < 3);
+
+                
+            
+
+            
+
+           
+
+
+           
+
+
+        }
+    }
+}
